4d48077e1d8ee209a38b2c2b70a8bcf89fbf18f4
JHolm <102568761+JonMunkholm@users.noreply.github.com>
2024-03-31 09:29:09 -0700

    add mongoDB and test both database and server are running

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,4 @@
 //replace with other config options
 export const PORT = 5000;
+
+export const mongoDBURL = `mongodb+srv://jmunkholm1:h5$$MNstKkbVj3w@finance-mern.qokq9cq.mongodb.net/?retryWrites=true&w=majority&appName=finance-MERN`;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -7,6 +8,16 @@ app.get("/", (request, response) => {
   console.log(request);
   return response.status(200).send("Hello World!!");
 });
-app.listen(PORT, () => {
-  console.log(`app is listening to port: ${PORT}`);
-});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log(`app connected to database`);
+
+    app.listen(PORT, () => {
+      console.log(`app is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
