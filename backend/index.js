c9d9052d57b52da2dab8f2d25bce5152368ce11f
JHolm <102568761+JonMunkholm@users.noreply.github.com>
2024-03-30 22:28:03 -0700

    Hello World!! - to port 5000

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Hello World!!");
+});
 app.listen(PORT, () => {
   console.log(`app is listening to port: ${PORT}`);
 });
