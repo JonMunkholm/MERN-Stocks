bf74fa2eb88f43bf14e84d12de2431d3f1df2422
JHolm <102568761+JonMunkholm@users.noreply.github.com>
2024-04-01 19:07:30 -0700

    removed axios

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,7 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Ticker } from "./models/tickerModel.js";
-import axios from "axios";
 
 const app = express();
 
@@ -15,37 +14,37 @@ app.get("/", (request, response) => {
   console.log(request);
   return response.status(200).send("Hello World!!");
 });
+//
+// app.get(`/api/tickers`, async (request, response) => {
+//   try {
+//     const tickers = await Ticker.find({});
+//     response.status(200).json(tickers);
+//   } catch (error) {
+//     response.status(500).json({ message: error.message });
+//   }
+// });
 
-app.get(`/api/tickers`, async (request, response) => {
-  try {
-    const tickers = await Ticker.find({});
-    response.status(200).json(tickers);
-  } catch (error) {
-    response.status(500).json({ message: error.message });
-  }
-});
+// app.post(`/api/tickers`, async (request, response) => {
+//   try {
+//     if (!request.body.cik_str || !request.body.title || !request.body.ticker) {
+//       return response
+//         .status(400)
+//         .json({ message: `Send all required fields: cik, title, ticker` });
+//     }
+//     const newTicker = {
+//       cik_str: request.body.cik_str,
+//       title: request.body.title,
+//       ticker: request.body.ticker,
+//     };
 
-app.post(`/api/tickers`, async (request, response) => {
-  try {
-    if (!request.body.cik_str || !request.body.title || !request.body.ticker) {
-      return response
-        .status(400)
-        .json({ message: `Send all required fields: cik, title, ticker` });
-    }
-    const newTicker = {
-      cik_str: request.body.cik_str,
-      title: request.body.title,
-      ticker: request.body.ticker,
-    };
+//     const tick = await Ticker.create(newTicker);
 
-    const tick = await Ticker.create(newTicker);
-
-    return response.status(200).json(tick);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).json({ message: error.message });
-  }
-});
+//     return response.status(200).json(tick);
+//   } catch (error) {
+//     console.log(error.message);
+//     response.status(500).json({ message: error.message });
+//   }
+// });
 
 mongoose
   .connect(mongoDBURL)
